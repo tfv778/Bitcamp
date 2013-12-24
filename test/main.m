656c662d6b0ecd6bdf23e34d73e1f4d2ba613fa3
Qian Shao <qianshao@qians-MacBook-Air.local>
2013-12-24 18:53:22 +0800

    Initial Commit

---
+++ test/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  test
+//
+//  Created by Qian Shao on 13-12-24.
+//  Copyright (c) 2013年 qian shao. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
